c031d06698dc19065a6fba56cfb9df4f6df468da
NilsG-S <nils.griswold.steiner@gmail.com>
2018-03-12 13:02:00 -0500

    Added New function

--- crawler/crawler.go
+++ crawler/crawler.go
@@ -1,5 +1,12 @@
 package crawler
 
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
 // TODO: maybe have the crawler put the response bodies in a chan of io.Reader
 // Then let the consumer handle file saving
 
@@ -15,23 +22,54 @@ type Crawler struct {
 	// ID of the content HTML element
 	// Doesn't always work with id, img, and src
 	// TODO: future improvement?
-	content     string
-	contentAttr string
+	content string
+	// contentAttr string
 	// Do I need something to handle file naming?
 	// If I want a count, I'll need to mutex it
+	// TODO: Add stop methods (number of downloads, linked page not the same, re-encounter page, etc.)
+	// TODO: Use embedding with interfaces to allow functionality customization
 }
 
-/*
-	New
-	This function takes the following input:
-	- Initial target html page
-	- ID of the next link
-	- ID of the content link
+func New(init, next, attr, cont string) (*Crawler, error) {
+	var err error
+	var ok bool
+	var out *Crawler = &Crawler{
+		next:     next,
+		nextAttr: attr,
+		content:  cont,
+		ranges:   false,
+		targets:  make(chan string, 128),
+	}
 
-	It determines whether ranges is true or false
-	by targeting the content link. It then puts
-	the initial target into targets.
-*/
+	var doc *goquery.Document
+	doc, err = goquery.NewDocument(init)
+	if err != nil {
+		return nil, fmt.Errorf("Couldn't get initial page: %v", err)
+	}
+
+	// Get content URL
+	var url string
+	url, ok = doc.Find(cont).First().Attr("src")
+	if !ok {
+		return nil, fmt.Errorf("Couldn't get content source")
+	}
+
+	// Checking for "Accepts-Ranges"
+	var res *http.Response
+	res, err = http.Head(url)
+	if err != nil {
+		return nil, fmt.Errorf("Couldn't make HEAD request: %v", err)
+	}
+
+	if v := res.Header.Get("Accepts-Ranges"); v != "" {
+		out.ranges = true
+	}
+
+	// Put initial html page into targets
+	target <- init
+
+	return out, nil
+}
 
 // A function to spin off go routines for each new target
 
